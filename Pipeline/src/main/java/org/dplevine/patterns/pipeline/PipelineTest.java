dc921bf4fefd93bfaca6aa3724dc0464a428326e
dplevine <dplevine@gmail.com>
2023-10-02 21:52:16 -0400

    fixed a bug in PipelineTest

--- Pipeline/src/main/java/org/dplevine/patterns/pipeline/PipelineTest.java
+++ Pipeline/src/main/java/org/dplevine/patterns/pipeline/PipelineTest.java
@@ -6,7 +6,9 @@ public class PipelineTest implements Stage, StageBuilder {
     public static void main(String args[]) {
         try {
             pipeline = PipelineBuilder.createBuilder().buildFromPathName("/Users/dplevine/Desktop/example 3.json");
-            ExecutionContext context = pipeline.run();
+            ExecutionContext context = new ExecutionContext();
+            context.addObject("pipeline", pipeline);
+            pipeline.run(context);
             pipeline.render("/Users/dplevine/Desktop/example", Pipeline.ImageType.GIF);
             System.out.println(context.getEventLog());
             System.out.println(context.getStatus());
@@ -18,6 +20,7 @@ public class PipelineTest implements Stage, StageBuilder {
     @Override
     public ExecutionContext doWork(ExecutionContext context) throws Exception {
         Thread.sleep(1000);
+        ((Pipeline)context.getObject("pipeline")).render("/Users/dplevine/Desktop/temp", Pipeline.ImageType.GIF);
         return context;
     }
 
